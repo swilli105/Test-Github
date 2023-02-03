9b478d91caf2ba7a80fe862ba719381ec54237cd
Shanice W <Shanice_Williams01@yahoo.com>
2023-02-03 15:54:08 -0500

    Initial Commit

---
+++ Test Github/Test_GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_GithubApp.swift
+//  Test Github
+//
+//  Created by Shanice Williams on 2/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Test_GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
